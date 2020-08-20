08173fe776447bf2d7b8c51ba622990bf6c1cc23
Fjodors Pohodnevs <fjodors.pohodnevs@citadele.lv>
2020-08-20 18:26:29 +0300

    Initial commit

---
+++ app/src/main/java/lv/fjodors/android_rtu_practice/MainActivity.kt
@@ -0,0 +1,11 @@
+package lv.fjodors.android_rtu_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
